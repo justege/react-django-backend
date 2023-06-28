d3a20f4ebaa265d121359e7a9bfc425952b99ebd
justege <okuregemen1@gmail.com>
2023-06-28 21:57:26 +0200

    db

--- softapyBackend/settings.py
+++ softapyBackend/settings.py
@@ -126,7 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # Updated STATIC_URL
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Added STATIC_ROOT
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
